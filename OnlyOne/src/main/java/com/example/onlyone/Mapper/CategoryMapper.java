64f7f56b2bb7e1bd402d6ae9d8878001b638b52d
ExtremeNina <bochi22@126.com>
2026-06-02 12:41:20 +0800

    新曾功能： 1，推荐系统 2.ai智能审核

--- OnlyOne/src/main/java/com/example/onlyone/Mapper/CategoryMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.onlyone.Mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.onlyone.Entity.Category;
-
-public interface CategoryMapper extends BaseMapper<Category> {
-}
--- OnlyOne/src/main/java/com/example/onlyone/Mapper/LoveTypeMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.onlyone.Mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.onlyone.Entity.LoveType;
-
-public interface LoveTypeMapper extends BaseMapper<LoveType> {
-}
